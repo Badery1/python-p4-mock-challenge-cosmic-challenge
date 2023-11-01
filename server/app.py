396842974fd2eecda11d8518b473ef961db1492b
Michael Myers <mmyers.com@Gmail.com>
2023-11-01 13:21:02 -0500

    complete mock CC

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from models import db, Scientist, Mission, Planet
 from flask_restful import Api, Resource
 from flask_migrate import Migrate
@@ -20,10 +18,186 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
 
 @app.route('/')
 def home():
     return ''
+
+class Scientists(Resource):
+
+    def get(self):
+
+        scientists = Scientist.query.all()
+
+        scientists_dict = [scientist.to_dict(rules = ('-missions', )) for scientist in scientists]
+
+        response = make_response(
+            jsonify(scientists_dict),
+            200
+        )
+
+        return response
+    
+    def post(self):
+
+        data = request.get_json()
+
+        try:
+
+            new_scientist = Scientist(
+                name = data['name'],
+                field_of_study = data['field_of_study']
+            )
+
+            db.session.add(new_scientist)
+            
+            db.session.commit()
+
+            response = make_response(
+                jsonify(new_scientist.to_dict(rules = ('-missions', ))),
+                201
+            )
+
+        except ValueError:
+
+            response = make_response(
+                { "errors": ["validation errors"] },
+                400
+            )
+
+        return response
+    
+api.add_resource(Scientists, '/scientists')
+
+@app.route('/scientists/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
+def scientist_by_id(id):
+
+    scientist = Scientist.query.filter(Scientist.id == id).first()
+
+    if request.method == 'DELETE':
+
+        if scientist:
+
+            missions = Mission.query.filter(Mission.scientist_id == id).all()
+
+            for mission in missions:
+
+                db.session.delete(mission)
+
+            db.session.delete(scientist)
+
+            db.session.commit()
+
+            response = make_response(
+                {},
+                204
+            )
+
+            return response
+
+        else:
+
+            response = make_response(
+                { "error": "Scientist not found" },
+                404
+            )
+
+
+        return response
+
+
+
+    if scientist:
+
+        if request.method == 'GET':
+
+            scientist_dict = scientist.to_dict()
+
+            response = make_response(
+                jsonify(scientist_dict),
+                200
+            )
+
+        elif request.method == 'PATCH':
+
+            data = request.get_json()
+
+            try:
+
+                for key in data:
+                    setattr(scientist, key, data[key])
+
+                db.session.add(scientist)
+
+                db.session.commit()
+
+                response = make_response(
+                    jsonify(scientist.to_dict(rules = ('-missions', ))),
+                    202
+                )
+
+            except ValueError:
+
+                response = make_response(
+                    { "errors": ["validation errors"] },
+                    400
+                )
+
+
+    else:
+
+        response = make_response(
+            { "error": "Scientist not found" },
+            404
+        )
+
+    return response
+
+@app.route('/planets', methods = ['GET'])
+def planets():
+
+    planets = Planet.query.all()
+
+    planets_dict = [planet.to_dict(rules = ('-missions', )) for planet in planets]
+
+    response = make_response(
+        jsonify(planets_dict),
+        200
+    )
+
+    return response
+
+@app.route('/missions', methods = ['POST'])
+def missions():
+
+    data = request.get_json()
+
+    try:
+
+        new_mission = Mission(
+            name = data['name'],
+            scientist_id = data['scientist_id'],
+            planet_id = data['planet_id']
+        )
+
+        db.session.add(new_mission)
+
+        db.session.commit()
+
+        response = make_response(
+            jsonify(new_mission.to_dict()),
+            201
+        )
+
+    except ValueError:
+
+        response = make_response(
+            { "errors": ["validation errors"] },
+            400
+        )
+
+    return response
 
 
 if __name__ == '__main__':
